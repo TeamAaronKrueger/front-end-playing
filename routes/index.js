ea507b6ee56d4f9a7c2b82fa7b0c74f65f71003a
Simran Khosla <email.simrankhosla@gmail.com>
2016-01-06 14:28:35 -0600

    pushing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var model = require('../models/Deck');
+var modelCards = require('../models/Card');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  res.render('pages/welcome'); 
 });
 
 router.get('/mydecks', function(req, res, next) {
@@ -23,6 +24,26 @@ router.get('/mydecks', function(req, res, next) {
         currentUser: req.user});
       });
     });
+
+    router.get('/editdeck', function(req, res, next) {
+      var currentDeck = "ChemistryQuiz"
+      modelCards.find(function(error, cards){
+          if (error) console.log(error);
+          // var currentCards = [];
+          // for (var card in cards) {
+          //   if (card.DeckName == currentDeck) {
+          //     currentCards.push(card);
+          //   };
+          // };
+          // console.log(currentDecks)
+          res.render('pages/editdeckview', {
+            cards: cards,
+            currentUser: req.user,
+            currentDeck: currentDeck
+            });
+          });
+        });
+
 
 
 
